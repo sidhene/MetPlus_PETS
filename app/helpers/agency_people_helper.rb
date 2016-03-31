98bf02186338788b7fa41be88686c2999c52ca15
Amy Lightholder <sidhene@gmail.com>
2016-03-31 09:44:49 -0700

    navbar for JDhome

--- app/helpers/agency_people_helper.rb
+++ app/helpers/agency_people_helper.rb
@@ -52,4 +52,10 @@ module AgencyPeopleHelper
     end
     seekers
   end
+  def current_user_job_developer?
+    return false unless user_signed_in?
+    return false unless ((person = current_user.actable).is_a? AgencyPerson)
+    person.agency_roles.pluck(:role).include? AgencyRole::ROLE[:JD]
+  end
+
 end
